c1ba3036764173f677e099b568a71b207b130ba8
Prem Kumar Rasakonda <117320898+prem8667@users.noreply.github.com>
2024-05-20 20:31:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
-
+ 
 ## Function To get response from LLAma 2 model
 
 def getLLamaresponse(input_text,no_words,blog_style):
